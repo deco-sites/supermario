fa1e3d4023312d99be3f75acfcb16089a3691f97
Abraão Morais <moraisabraao@gmail.com>
2023-03-25 23:48:27 -0300

    Update HeroBanner.tsx

--- components/organisms/HeroBanner.tsx
+++ components/organisms/HeroBanner.tsx
@@ -8,6 +8,12 @@ export type Props = {
   imageProps: ImageProps;
 };
 
+function loadIframe(){
+    return(
+        <iframe class="object-contain w-full" width="560" height="315" src="https://www.youtube.com/embed/L7Jt4BdAOE0?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>
+    )
+}
+
 export default function HeroBanner({ imageProps, mainMenu }: Props) {
   return (
     <div class="w-full pt-12 p-4 flex flex-col justify-center items-center bg-[#f3cc41]">
@@ -22,7 +28,7 @@ export default function HeroBanner({ imageProps, mainMenu }: Props) {
         Consertar canos é a nossa diversão!
       </h3>
       <div class="w-[400px]">
-        <iframe class="object-contain w-full" width="560" height="315" src="https://www.youtube.com/embed/L7Jt4BdAOE0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        {loadIframe()}
       </div>
       
       <NavMain/>

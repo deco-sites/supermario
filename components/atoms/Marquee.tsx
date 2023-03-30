f3d7e9310f877d60f8f02fbab503cb45484280a7
Abraão Morais <moraisabraao@gmail.com>
2023-03-30 01:18:00 -0300

    feat marquee

---
+++ components/atoms/Marquee.tsx
@@ -0,0 +1,15 @@
+export interface Props {
+  /** @description Qual mensagem deseja exibir no banner de texto rotativo */
+  menssage?: string;
+}
+
+export default function Marquee({menssage="Visite-nos em smbencanadores.com.br"}:Props) {
+  return (
+    <div class="w-full max-w-full inline-block overflow-hidden bg-[#f81621] relative text-[#fff] py-2">
+      <div id="marquee" class="animate-text-Marquee overflow-hidden whitespace-nowrap inline-flex uppercase text-[2.5rem] lg:text-[4rem] font-['roc-grotesk',sans-serif]">
+        <p class="px-12">{menssage}</p>
+        <p class="px-12">{menssage}</p>
+      </div>
+    </div>
+  );
+}
---
+++ sections/Marquee.tsx
@@ -0,0 +1,5 @@
+import Marquee,{Props} from "$start/components/atoms/Marquee.tsx";
+
+export default function MarqueeSection(props:Props){
+    return <Marquee {...props}/>
+}

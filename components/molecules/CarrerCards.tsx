7bdea45bad4b3c7fde4cac8e574637228b82f71a
Abraão Morais <moraisabraao@gmail.com>
2023-04-02 17:50:57 -0300

    fix best practices metric

--- components/molecules/CarrerCards.tsx
+++ components/molecules/CarrerCards.tsx
@@ -11,7 +11,7 @@ export default function CarrerCard({cargo,description,src}: CarrerCardProps) {
   return (
     <div class="w-full max-w-[80%] h-auto bg-white flex flex-col justify-start items-center border-2 py-8 px-6 border-[#f81621] relative text-center lg:(max-w-[268px] h-72  text-left p-4 )">
       <img
-        class="w-[100px] h-24  rounded-1/2 border-0 border-[#f81621] absolute -top-[50px]"
+        class="w-[100px] h-[100px]  rounded-1/2 border-0 border-[#f81621] absolute -top-[50px]"
         src={src}
         loading="lazy"
         preload="false"

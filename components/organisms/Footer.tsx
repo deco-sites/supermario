b78fa11c9a0b1e983c07d007e18d41bfd6eb1cc9
Abraão Morais <moraisabraao@gmail.com>
2023-04-02 17:33:20 -0300

    Update Footer.tsx

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer({ navItem }: footerProps) {
   return (
     <footer class="w-full flex flex-col justify-center items-center bg-[#f3cc41] gap-12 lg:p-20">
       <span id="line-divider" class="w-full border-[#f81621] border-[2px]" />
-      <nav class="flex flex-col gap-6 px-12 lg:(flex-row flex-wrap justify-center items-center)">
-        <ul>
+      <nav>
+        <ul class="flex flex-col gap-6 px-12 lg:(flex-row flex-wrap justify-center items-center)">
           {navItem?.map((menuItem) => (
           <NavItem
             className="!max-w-[300px]"
